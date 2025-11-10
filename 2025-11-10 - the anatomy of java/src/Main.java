a43a5b08214c08804fb5a936faa972b3457c8d5a
0tieno <ronvi035@gmail.com>
2025-11-10 14:48:23 +0300

    third commit

--- 2025-11-10 - the anatomy of java/src/Main.java
+++ 2025-11-10 - the anatomy of java/src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World! Here is my first Java program.");
+        System.out.println("Hello, World! Here is my first Java program. I'm learning the anatomy of a Java program.");
     }
 }
