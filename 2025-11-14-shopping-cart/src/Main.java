d841469741aaf2532e45f4ba7f5ac67101e08a47
0tieno <ronvi035@gmail.com>
2025-11-14 13:17:59 +0300

    Add .gitignore and project configuration files

---
+++ 2025-11-14-shopping-cart/src/Main.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        printReceipt(args);
+    }
+
+    public static void printReceipt(String[] args){
+        String itemName;
+        double itemQuantity;
+        double itemPrice;
+        double totalPrice;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter item name: ");
+        itemName = scanner.nextLine();
+        System.out.print("Enter item quantity: ");
+        itemQuantity = scanner.nextDouble();
+        System.out.print("Enter item price: ");
+        itemPrice = scanner.nextDouble();
+
+        totalPrice = itemPrice * itemQuantity;
+
+        System.out.println("You have bought " + itemQuantity + " " + itemName + "(s) at a price of $" + itemPrice + " each.");
+        System.out.println("Total price: $" + totalPrice);
+
+        scanner.close();
+    }
+}
