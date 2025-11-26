b4bbd3e8e2bcdb421843b18614c1d8dd7eac47ca
0tieno <ronvi035@gmail.com>
2025-11-26 21:16:27 +0300

    done

---
+++ 2025-11-26-area-of-a-rectangle/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args){
+        double length = 5.0;
+        double width = 3.0;
+        double area = length * width;
+        System.out.println("The area of the rectangle is " + area + "cm²");
+    }
+}
+
+// This program calculates the area of a rectangle given its length and width.
+// It then prints the area to the console.
