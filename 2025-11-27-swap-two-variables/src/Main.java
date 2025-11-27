eabf7734fa4ec987e88eb66ffd70be9a0cab4fcf
0tieno <ronvi035@gmail.com>
2025-11-27 08:37:30 +0300

    finished

---
+++ 2025-11-27-swap-two-variables/src/Main.java
@@ -0,0 +1,30 @@
+public class Main {
+    public static void main (String[] args){
+//        String firstVariable = "world!";
+//        String secondVariable = "Hello, ";
+//
+//        String swappedFirstVariable = secondVariable;
+//        String swappedSecondVariable = firstVariable;
+//
+//        String swappedVariable = swappedFirstVariable + swappedSecondVariable;
+//        System.out.println(swappedVariable);
+
+//        int a= 10;
+//        int b= 5;
+//
+//        int temp = a;
+//        a = b;
+//        b = temp;
+//
+//        System.out.println("a = " + a + ", b = " + b);
+
+        String a = "world!";
+        String b = "Hello";
+
+        String temp = a;
+        a = b;
+        b = temp;
+
+        System.out.println("a = " + a + ", b = " + b);
+    }
+}
