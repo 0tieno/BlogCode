dcaabfe65a3ff629aac457148fa828a65e5ca06c
0tieno <ronvi035@gmail.com>
2025-11-28 07:22:29 +0300

    finished

---
+++ 2025-11-28-square-root/src/Main.java
@@ -0,0 +1,11 @@
+//import static java.lang.Math.sqrt;
+
+public class Main {
+    public static void main(String[] args){
+        double myNumber = 9;
+        double result = Math.sqrt(myNumber);
+        System.out.println(result);
+
+
+    }
+}
