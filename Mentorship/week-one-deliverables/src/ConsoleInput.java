f99d1bcf57c8092ded856b2e45d5dcbf68e9feaf
0tieno <ronvi035@gmail.com>
2026-01-12 18:00:18 +0300

    add demos

--- Mentorship/week-one-deliverables/src/ConsoleInput.java
+++ Mentorship/week-one-deliverables/src/ConsoleInput.java
@@ -1,2 +1,23 @@
+import java.util.Scanner;
+
 public class ConsoleInput {
+
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+//        System.out.print("Please enter your name:");
+//
+//        String name = scanner.nextLine();
+//        System.out.println("Hello, " + name + "! Welcome to the Java world.");
+
+        System.out.println("enter number:");
+        int num = scanner.nextInt();
+
+        if (num % 2 == 0){
+            System.out.println(num + " is even number");
+        }else{
+            System.out.println(num + " is odd number");
+        }
+
+        scanner.close();
+    }
 }
