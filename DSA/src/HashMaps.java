7320fb252a8013b7191ad02892f0279e48b5a208
0tieno <ronvi035@gmail.com>
2026-03-08 22:37:30 +0300

    small change

--- DSA/src/HashMaps.java
+++ DSA/src/HashMaps.java
@@ -14,21 +14,20 @@ public class HashMaps {
          * values();
          *
          * A HashMap stores items in key/value pairs, where each key maps to a specific value.
-         * It is part of the java.util package and implements the Map interface.
          * Instead of accessing elements by an index (like with ArrayList), you use a key to retrieve its associated value.
          * A HashMap can store many different combinations, such as:
          * String keys and Integer values
          * String keys and String values
          */
-      HashMap<String, String> capitalCities = new HashMap<>();
-
-        capitalCities.put("Kenya", "Nairobi");
-        capitalCities.put("England", "London");
-        capitalCities.put("India", "New Dehli");
-        capitalCities.put("Austria", "Wien");
-        capitalCities.put("Norway", "Oslo");
-        capitalCities.put("Norway", "Oslo"); // Duplicate
-        capitalCities.put("USA", "Washington DC");
+//      HashMap<String, String> capitalCities = new HashMap<>();
+//
+//        capitalCities.put("Kenya", "Nairobi");
+//        capitalCities.put("England", "London");
+//        capitalCities.put("India", "New Dehli");
+//        capitalCities.put("Austria", "Wien");
+//        capitalCities.put("Norway", "Oslo");
+//        capitalCities.put("Norway", "Oslo"); // Duplicate
+//        capitalCities.put("USA", "Washington DC");
 
 //        System.out.println(capitalCities.get("Kenya"));
 //        System.out.println(capitalCities.remove("Australia"));
@@ -45,14 +44,22 @@ public class HashMaps {
 
 //        System.out.println(capitalCities);
 
-        HashMap<String, Integer> People = new HashMap<>();
+//        HashMap<String, Integer> People = new HashMap<>();
+//
+//        People.put("ron", 23);
+//        People.put("steve", 23);
+//        People.put("brian", 23);
+//
+//        System.out.println(People);
+//        System.out.println(People.size());
 
-        People.put("ron", 23);
-        People.put("steve", 23);
-        People.put("brian", 23);
+        HashMap<String, String> Students = new HashMap<>();
 
-        System.out.println(People);
-        System.out.println(People.size());
+        Students.put("stream", "green");
+        Students.put("name", "joe");
+
+        System.out.println(Students);
+
 
 
     }
