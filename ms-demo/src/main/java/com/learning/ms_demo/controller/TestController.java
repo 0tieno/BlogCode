f27041ddaaa43de29f396f0049af8784a6f288dd
0tieno <ronvi035@gmail.com>
2026-04-07 13:55:18 +0300

    add endpoints for student management in TestController

--- ms-demo/src/main/java/com/learning/ms_demo/controller/TestController.java
+++ ms-demo/src/main/java/com/learning/ms_demo/controller/TestController.java
@@ -1,14 +1,40 @@
 package com.learning.ms_demo.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.learning.ms_demo.Student;
+import com.learning.ms_demo.repository.StudentRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class TestController {
 
-    @GetMapping("/test")
-    public String helloWorld(){
-        return "Hello world";
+    private final StudentRepository studentRepository;
+
+    public TestController(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
     }
+
+    @PostMapping("/students")
+    public Student postStudent(@RequestBody Student student){
+        return studentRepository.save(student);
+    }
+
+    @GetMapping("/students")
+    public List<Student> getAllStudents(){
+        return studentRepository.findAll();
+    }
+
+    @GetMapping("/students/{student-id}")
+    public Student getStudentById(@PathVariable("student-id") Integer Id){
+        return studentRepository.findById(Id).orElse(null);
+    }
+
+    @GetMapping("/students/search/{student-name}")
+    public List<Student> getStudentByFirstName(@PathVariable("student-name") String name){
+        return studentRepository.findAllByFirstnameContaining(name);
+    }
+
+
 
 }
