056821751e836d3444ffa91ea1a29f365a005e0b
0tieno <ronvi035@gmail.com>
2026-04-27 17:13:18 +0300

    implement create, update, and delete operations in PostServiceImpl

--- Mentorship/blogging-platform-api/src/main/java/com/kampuni/blogging_platform_api/service/PostServiceImpl.java
+++ Mentorship/blogging-platform-api/src/main/java/com/kampuni/blogging_platform_api/service/PostServiceImpl.java
@@ -2,12 +2,17 @@ package com.kampuni.blogging_platform_api.service;
 
 import com.kampuni.blogging_platform_api.dto.PostRequest;
 import com.kampuni.blogging_platform_api.dto.PostResponse;
+import com.kampuni.blogging_platform_api.entity.Post;
 import com.kampuni.blogging_platform_api.repository.PostRespository;
+import jakarta.validation.Valid;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
+@Service
 public class PostServiceImpl implements PostService{
-
 
     private final PostRespository postRespository;
 
@@ -17,26 +22,71 @@ public class PostServiceImpl implements PostService{
 
     @Override
     public PostResponse createPost(PostRequest postRequest) {
-        return null;
+
+        Post post = new Post();
+
+        post.setTitle(postRequest.getTitle());
+        post.setContent(postRequest.getContent());
+        post.setCategory(postRequest.getCategory());
+        post.setTags(postRequest.getTags());
+
+        post.setCreatedAt(LocalDateTime.now());
+        post.setUpdatedAt(LocalDateTime.now());
+
+        Post savedPost = postRespository.save(post);
+        return mapToResponse(savedPost);
+
     }
 
     @Override
     public List<PostResponse> getAllPost() {
-        return List.of();
+        return postRespository.findAll().stream().map(this::mapToResponse).toList();
     }
 
     @Override
     public PostResponse getOne(Long id) {
-        return null;
+        Post post = postRespository.findById(id).orElseThrow(() -> new RuntimeException("post not found"));
+        return mapToResponse(post);
     }
 
     @Override
-    public PostResponse updatePost(Long id, PostRequest postRequest) {
-        return null;
+    public PostResponse updatePost(
+            Long id,
+            @Valid @RequestBody PostRequest postRequest) {
+
+        Post post = new Post();
+
+        post.setTitle(postRequest.getTitle());
+        post.setContent(postRequest.getContent());
+        post.setCategory(postRequest.getCategory());
+        post.setTags(postRequest.getTags());
+        post.setUpdatedAt(LocalDateTime.now());
+
+        Post updatedPost = postRespository.save(post);
+
+        return mapToResponse(updatedPost);
+
+
     }
 
     @Override
     public void deletePost(Long id) {
+        postRespository.deleteById(id);
+    }
+
+    private PostResponse mapToResponse(Post post){
+
+        PostResponse postResponse = new PostResponse();
+
+        postResponse.setId(post.getId());
+        postResponse.setTitle(post.getTitle());
+        postResponse.setContent(post.getContent());
+        postResponse.setCategory(post.getCategory());
+        postResponse.setTags(post.getTags());
+        postResponse.setCreatedAt(post.getCreatedAt());
+        postResponse.setUpdatedAt(post.getUpdatedAt());
+
+        return postResponse;
 
     }
 }
