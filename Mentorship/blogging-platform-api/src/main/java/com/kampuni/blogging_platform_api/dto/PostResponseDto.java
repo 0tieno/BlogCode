c0086f472ffb6f5541495c469521c83ba734c448
0tieno <ronvi035@gmail.com>
2026-05-01 10:02:06 +0300

    refactor: convert PostResponseDto to a record for improved immutability and conciseness

--- Mentorship/blogging-platform-api/src/main/java/com/kampuni/blogging_platform_api/dto/PostResponseDto.java
+++ Mentorship/blogging-platform-api/src/main/java/com/kampuni/blogging_platform_api/dto/PostResponseDto.java
@@ -1,21 +1,16 @@
 package com.kampuni.blogging_platform_api.dto;
 
-import lombok.Getter;
-import lombok.Setter;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
-@Getter
-@Setter
-public class PostResponseDto {
 
-    private Long id;
-    private String title;
-    private String content;
-    private String category;
-    private List<String> tags;
-
-    private LocalDateTime createdAt;
-    private LocalDateTime updatedAt;
-}
+public record PostResponseDto(
+        Long id,
+        String title,
+        String content,
+        String category,
+        List<String> tags,
+        LocalDateTime createdAt,
+        LocalDateTime updatedAt
+) {}
