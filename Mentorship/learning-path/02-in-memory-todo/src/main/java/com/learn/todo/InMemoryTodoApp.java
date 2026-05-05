944ee237b113f5970b8f91c212d9d43f44abe40d
0tieno <ronvi035@gmail.com>
2026-05-05 09:21:10 +0300

    feat: add InMemoryTodoApp and JwtService for JWT token management

---
+++ Mentorship/learning-path/02-in-memory-todo/src/main/java/com/learn/todo/InMemoryTodoApp.java
@@ -0,0 +1,12 @@
+package com.learn.todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InMemoryTodoApp {
+    public static void main(String[] args) {
+        SpringApplication.run(InMemoryTodoApp.class, args);
+    }
+}
+
