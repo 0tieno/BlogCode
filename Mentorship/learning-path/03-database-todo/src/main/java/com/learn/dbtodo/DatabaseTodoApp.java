802e313627b4c6ac6bdc7dbc2a00afd3f8ae1930
0tieno <ronvi035@gmail.com>
2026-05-05 09:15:10 +0300

    feat: add main application class for DatabaseTodoApp

---
+++ Mentorship/learning-path/03-database-todo/src/main/java/com/learn/dbtodo/DatabaseTodoApp.java
@@ -0,0 +1,12 @@
+package com.learn.dbtodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseTodoApp {
+    public static void main(String[] args) {
+        SpringApplication.run(DatabaseTodoApp.class, args);
+    }
+}
+
