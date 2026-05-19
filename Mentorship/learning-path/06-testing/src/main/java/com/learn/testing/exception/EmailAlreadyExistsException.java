578b2a993e56b0546ff0664345bd4582f74f8aba
0tieno <ronvi035@gmail.com>
2026-05-19 11:26:41 +0300

    feat: create EmailAlreadyExistsException for user registration conflicts

---
+++ Mentorship/learning-path/06-testing/src/main/java/com/learn/testing/exception/EmailAlreadyExistsException.java
@@ -0,0 +1,6 @@
+package com.learn.testing.exception;
+
+public class EmailAlreadyExistsException extends RuntimeException {
+    public EmailAlreadyExistsException(String email) { super("Email already exists: " + email); }
+}
+
