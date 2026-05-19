49a74b2ac9fb4f67b7b948291302c6b9380adfcc
0tieno <ronvi035@gmail.com>
2026-05-19 14:41:48 +0300

    feat: enhance ToDo entity with fields for id, title, description, completed status, and createdAt timestamp

--- Mentorship/todoapi/src/main/java/com/learn/todoapi/entity/ToDo.java
+++ Mentorship/todoapi/src/main/java/com/learn/todoapi/entity/ToDo.java
@@ -1,4 +1,40 @@
 package com.learn.todoapi.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "todos")
+
 public class ToDo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    private String description;
+
+    @Column(nullable = false)
+    private boolean completed = false;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+
+    public ToDo() {
+    }
+
+    @PrePersist
+    public void prePersist(){
+        this.createdAt = LocalDateTime.now();
+    }
+
 }
