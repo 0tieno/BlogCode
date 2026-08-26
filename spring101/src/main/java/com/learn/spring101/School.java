b2967b6f22ef53ce7743c7958cddbad1b604ab1c
0tieno <ronvi035@gmail.com>
2026-08-26 18:05:59 +0300

    feat: add School entity with properties and relationships

---
+++ spring101/src/main/java/com/learn/spring101/School.java
@@ -0,0 +1,50 @@
+package com.learn.spring101;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+public class School {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+
+    @OneToMany(mappedBy = "school")
+    private List<Student>  student;
+
+
+    public School() {
+    }
+
+    public List<Student> getStudent() {
+        return student;
+    }
+
+    public void setStudent(List<Student> student) {
+        this.student = student;
+    }
+
+    public School(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
