96168e5fd9dd8d4f4ef64da4e52a82a382b2ff5a
0tieno <ronvi035@gmail.com>
2026-08-26 18:06:25 +0300

    feat: add StudentProfile entity with bio and relationship to Student

---
+++ spring101/src/main/java/com/learn/spring101/StudentProfile.java
@@ -0,0 +1,40 @@
+package com.learn.spring101;
+
+
+import jakarta.persistence.*;
+
+@Entity
+public class StudentProfile {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String bio;
+
+    @OneToOne
+    @JoinColumn(name = "student_id")
+    private Student student;
+
+    public StudentProfile() {
+    }
+
+    public StudentProfile(Long id, String bio) {
+        this.id = id;
+        this.bio = bio;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+}
